816dc83e434662723982b539f0e467f051501505
realaravinth <realaravinth@batsense.net>
2021-08-24 10:55:31 +0530

    build script

--- Makefile
+++ Makefile
@@ -1,7 +1,13 @@
 default:
-	agate --content content/ \
+	agate --content dist/content/ \
        --certs ./certs/ \
        --addr [::]:1965 \
        --addr 0.0.0.0:1965 \
        --hostname localhost \
        --lang en-US
+
+clean: 
+	./scripts/build.sh clean
+
+prod: clean
+	./scripts/build.sh build
---
+++ scripts/build.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+# CI Build script
+# Copyright © 2021 yourname
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+readonly CONTENT_DIR=content
+readonly DIST_DIR=dist
+
+help() {
+	cat << EOF
+build.sh: CI build script
+USAGE:
+    build.sh <options>
+OPTIONS:
+  b   build     build website
+  c   clean     clean dependencies and build artifacts
+  h   help      print this help menu
+EOF
+}
+
+check_arg(){
+	if [ -z $1 ]
+	then
+		help
+		exit 1
+	fi
+}
+
+match_arg() {
+	if [ $1 == $2 ] || [ $1 == $3 ]
+	then
+		return 0
+	else
+		return 1
+	fi
+}
+
+clean() {
+	rm -rf $DIST_DIR || true
+}
+
+last_mod() {
+	git --no-pager log -1 --pretty="format:%ci" $1
+	git --no-pager log -1 --pretty="format:Last modified: %ci%n" content/index.gmi
+}
+
+prep_dist() {
+	for i in $(find $CONTENT_DIR -type d)
+	do
+		mkdir -p $DIST_DIR/$i
+	done
+}
+
+build() {
+	prep_dist
+	for file in $(find $CONTENT_DIR -type f)
+	do 
+		readonly NEW_FILE=$DIST_DIR/$file
+		cat $file > $NEW_FILE
+		echo >> $NEW_FILE
+		echo "--------" >> $NEW_FILE
+		echo >> $NEW_FILE
+		last_mod $file >> $NEW_FILE
+	done
+}
+
+check_arg $1
+
+if match_arg $1 'b' 'build'
+then
+	build
+elif match_arg $1 'c' 'clean'
+then
+	clean
+elif match_arg $1 'h' 'help'
+then
+	help
+else
+	echo "Error: $1 is not an option"
+	help
+	exit 1
+fi
+
+exit 0
